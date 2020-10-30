3cb56d1c82a14f64925d0851d1c0d24cb4e3f50f
Michael19 <socolovmisha@mail.ru>
2020-10-30 18:10:35 +0300

    some

--- main.cpp
+++ main.cpp
@@ -8,10 +8,23 @@ int main()
 {
     PQueue<int> queue;
     queue.enqueue(1, 5);
+     std::cout << queue << std::endl;
     queue.enqueue(2, 7);
+     std::cout << queue << std::endl;
     queue.enqueue(4, 2);
+     std::cout << queue << std::endl;
     queue.enqueue(4, 6);
-    queue.enqueue(4, 8);
+     std::cout << queue << std::endl;
+    queue.enqueue(1, 8);
+
+    std::cout << queue << std::endl;
+
+    for (int i = 0; i < 1e5; ++i) {
+        queue.enqueue(std::rand() % 30, i);
+//         std::cout << queue << std::endl;
+    }
+
+     std::cout << queue << std::endl;
 
     return 0;
 }
--- pqueue.h
+++ pqueue.h
@@ -2,6 +2,8 @@
 #define PQUEUE_H
 
 #include <ostream>
+
+
 using namespace std;
 
 template<class T>
@@ -14,105 +16,179 @@ struct PR_Element{
 
     }
 
+    PR_Element<T>& operator =(const PR_Element<T>& other){
+        this->pointer_to_data = other.pointer_to_data;
+        this->priority = other.priority;
+        this->size = other.size;
+    }
+
+    bool is_empty()const{
+        return size == 0;
+    }
+
 
 };
 
 template<class T>
-ostream& operator<<(ostream& os, const PQueue<T>& dt)
-{
-    os << dt._size << std::endl;
-    return os;
-}
+class PQueue;
 
+template<class T>
+ostream& operator<<(ostream& os, const PQueue<T>& dt);
 
 template<class T>
 class PQueue{
 public:
+    template<T>
     friend ostream& operator<<(ostream& os, const PQueue<T>& dt);
 
 public:
-    PQueue():_data(nullptr), _priority(nullptr), _size(0){
+    PQueue(): _data(nullptr), _priority(nullptr), _size(0){
 
     }
 
     ~PQueue()
     {
-        if(_data)
-            delete[] _data;
-
         if(_priority)
             delete[] _priority;
+    }
+
+    void resize(){
+
+        PR_Element<T>* _priority_temp = nullptr;
+        _priority_temp =   new PR_Element<T>[_size + SIZE_OF_BLOCKS_PRIORITY];
+
+
+        for (int i = 0; i < _size; ++i) {
+            _priority_temp[i] = _priority[i];
+        }
+        for (int i = _size; i < _size+ SIZE_OF_BLOCKS_PRIORITY; ++i) {
+            T* data = new T[SIZE_OF_BLOCKS];
+            _priority_temp[i].pointer_to_data = data;
+        }
+
+        delete[] _priority;
+        _priority = _priority_temp;
+
+    }
+
+    void copy_priority(int new_pos){
+        PR_Element<T> new_elem = _priority[_size], buffer;
+        for (int i = 1; i < _size+1; ++i) {
+
+            if(i > new_pos){
+                //                if(!(_priority[i].is_empty()))
+                 buffer = _priority[i] ;
+
+                _priority[i] =_priority[new_pos];
+                _priority[new_pos] = buffer;
+                if(buffer.is_empty())
+                    break;
+            }
+        }
+        _priority[new_pos] = new_elem;
+
     }
 
     void enqueue(int priority, T item)
     {
         // init
-        if(!_data || !_priority){
-            if(_data)
-                delete[] _data;
-
-            if(_priority)
-                delete _priority;
-
-            _data = new T*[SIZE_OF_BLOCKS_PRIORITY];
+        if(!_priority){
             _priority = new PR_Element<T>[SIZE_OF_BLOCKS_PRIORITY];
+
+            for (int i = 0; i < SIZE_OF_BLOCKS_PRIORITY; ++i) {
+                T* data = new T[SIZE_OF_BLOCKS];
+                _priority[i].pointer_to_data = data;
+            }
         }
 
-//        if(!(_size % SIZE_OF_BLOCKS)){
-//            T* temp_data = new T[_size + SIZE_OF_BLOCKS];
-//            int* temp_priority = new int[_size + SIZE_OF_BLOCKS];
+        bool is_insert(false);
+        int new_pos = _size;
+        for (int j = 0; j < _size; ++j) {
+            if(_priority[j].priority < priority){
+                new_pos = j;
+                break;
+            }
 
-//            for (int i = 0; i < _size; ++i) {
-//                temp_data[i] = _data[i];
-//                temp_priority[i] = _priority[i];
-//            }
+            if(_priority[j].priority == priority){
+                is_insert = true;
+                new_pos = j;
+                break;
+            }
+        }
 
-//            delete[] _data;
-//            delete[] _priority;
+        std::cout << "new_pos = " << new_pos << std::endl;
 
-//            _data = temp_data;
-//            _priority = temp_priority;
-//        }
+        if(!(_size % SIZE_OF_BLOCKS_PRIORITY) && !is_insert && _size > 0){
+            // resize and copy data;
+
+            resize();
+            copy_priority(new_pos);
+        }
+        else if(!is_insert && new_pos < _size){
+            copy_priority(new_pos);
+        }
+
+        _priority[new_pos].pointer_to_data[_priority[new_pos].size] = item;
+        _priority[new_pos].size++;
+
+        if(!is_insert)
+        {
+            _priority[new_pos].priority = priority;
+            _size++;
+        }
+
+#if 0
+
 
         bool is_insert = false;// check if data into deque
-        int new_pos = _size+1;
-        for (int i = 0; i < _size-1; ++i) {
-            if(_priority[i].priority == priority){
-                // в очереди есть с таким приоритетом
+        int new_pos = _size;
+        if(new_pos != 0){
+            for (int i = 0; i < _size; ++i) {
+                if(_priority[i].priority == priority){
+                    // в очереди есть с таким приоритетом
 
-                if(!(_priority[i].size % SIZE_OF_BLOCKS)){
-                    T* temp_data = new T[_priority[i].size + SIZE_OF_BLOCKS];
+                    if(!(_priority[i].size % SIZE_OF_BLOCKS)){
+                        T* temp_data = new T[_priority[i].size + SIZE_OF_BLOCKS];
 
-                    for (int j = 0; j < _priority[i].size + SIZE_OF_BLOCKS; ++j) {
-                        temp_data[j] = _priority[i].pointer_to_data[j];
+                        for (int j = 0; j < _priority[i].size + SIZE_OF_BLOCKS; ++j) {
+                            temp_data[j] = _priority[i].pointer_to_data[j];
+                        }
+
+                        delete[] _priority[i].pointer_to_data;
+
+                        _priority[i].pointer_to_data = temp_data;
+
                     }
-
-                    delete[] _priority[i].pointer_to_data;
-
-                    _priority[i].pointer_to_data = temp_data;
-
+                    is_insert != true;
+                    break;
                 }
-                is_insert != true;
-                break;
-            }
-            else if(_priority[i].priority < priority){
-                // данные с наивысшем приоритетом
-                new_pos = 0;
-                break;
-            }
+                else if(_priority[i].priority < priority){
+                    // данные с наивысшем приоритетом
+                    new_pos = 0;
+                    break;
+                }
 
-            else if(_priority[i].priority > priority && _priority[i+1].priority < priority){
-                // данные с новым промежуточным приоритетом
-                new_pos = i+1;
-                break;
+                else if(_priority[i].priority > priority && _priority[i+1].priority < priority){
+                    // данные с новым промежуточным приоритетом
+                    new_pos = i+1;
+                    break;
+                }
             }
         }
 
         if(!is_insert)
         {
-            if(!(_size % SIZE_OF_BLOCKS_PRIORITY) || new_pos <= _size)
+            if(!(_size % SIZE_OF_BLOCKS_PRIORITY) || new_pos < _size)
             {
-                PR_Element<T>* _priority_temp = new PR_Element<T>[_size + SIZE_OF_BLOCKS_PRIORITY];
+                PR_Element<T>* _priority_temp = nullptr;
+                if(!(_size % SIZE_OF_BLOCKS_PRIORITY))
+                {
+                    _priority_temp =   new PR_Element<T>[_size + SIZE_OF_BLOCKS_PRIORITY];
+                }
+                else{
+                    _priority_temp =   new PR_Element<T>[int(_size / SIZE_OF_BLOCKS_PRIORITY) * SIZE_OF_BLOCKS_PRIORITY];
+                }
+                _size++;
 
                 for (int j = 0; j < _size; ++j)
                 {
@@ -121,49 +197,86 @@ public:
                         _priority_temp[j].pointer_to_data = new T[SIZE_OF_BLOCKS];
                         _priority_temp[j].size++;
                         _priority_temp[j].pointer_to_data[0] = item;
+                        _priority_temp[j].priority=priority;
                     }
                     else if(j > new_pos){
-                        _priority_temp[j] = _priority[j];
+                        _priority_temp[j] = _priority[j-1];
+                        std::cout << "_priority_temp[j].pointer_to_data[0] " << _priority_temp[j].pointer_to_data[0] << std::endl;
                     }
                     else{
-                        _priority_temp[j] = _priority[j+1];
+                        _priority_temp[j] = _priority[j];
                     }
                 }
+
+                delete[] _priority;
+                _priority = _priority_temp;
             }
-            else{
+            else
+            {
                 _priority[_size].pointer_to_data = new T[SIZE_OF_BLOCKS];
                 _priority[_size].size++;
                 _priority[_size].pointer_to_data[0] = item;
             }
         }
 
+#endif
+
     }
 
     T dequeue(){
-        if(_data && _priority && _size > 0){
-
-            T return_element = _data[0];
+        if(_priority && _size > 0){
 
 
+            T out = _priority[0].pointer_to_data[0];
 
-            return return_element;
+            T* temp_data = _priority[0].pointer_to_data;
+            _priority[0].pointer_to_data = temp_data[1];
+
+            delete temp_data;
+            return out;
+
+
         }
 
         return T();
     }
 
+    void print() const{
+        size_t s = _size;
+        for (int i = 0; i < s; ++i) {
+            size_t ss = _priority[i].size;
+            std::cout << _priority[i].priority << " : ";
+            for (int j = 0; j < ss; ++j) {
+                std::cout << _priority[i].pointer_to_data[j] << " ";
+            }
+            std::cout << std::endl;
+        }
+    }
+
+    size_t size()const{
+        return _size;
+    }
+
 private:
 
 
 
 private:
-    T** _data; // данные
+    T** _data;
     PR_Element<T>* _priority; // массив указателей на данные, отсортированный по приоритетам
     size_t _size; // количество приоритетов
 
-    const size_t SIZE_OF_BLOCKS = 10;
-    const size_t SIZE_OF_BLOCKS_PRIORITY = 10;
+    const size_t SIZE_OF_BLOCKS = 1000;
+    const size_t SIZE_OF_BLOCKS_PRIORITY = 20;
 };
+
+template<class T>
+ostream& operator<<(ostream& os, const PQueue<T>& dt)
+{
+    dt.print();
+
+    return os;
+}
 
 #endif // PQUEUE_H
 
